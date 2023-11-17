90d70c36ce12a55486bf912e9c674f2eac9e34bc
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2023-11-17 21:22:01 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,12 +1,9 @@
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int share = 0;
         for(int i=1; i <= n; i++){
             if(n % i == 0 ){
-                share = n / i;
-                answer += share;
+                answer += i;
             }
         } return answer;
     }
