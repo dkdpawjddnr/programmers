d1a54eaffc4993016d43ee12596613ef2b1d7ee7
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2024-09-25 17:38:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 264 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
 
-        for (int i = 1; i<= number; i++){
-            for (int j = number -i; j>0; j--){
-                System.out.print(" ");
-            }
-            for (int k=0;k<i;k++){
-                System.out.print("*");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        for(int i = 1; i<=a; i++){
+            for(int j = a; j>0; j--){
+                if(i<j){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
-        scanner.close();
     }
 }
