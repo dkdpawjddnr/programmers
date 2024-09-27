006c2a5260e177b604d38f3aa730cea5aa602272
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2024-09-28 00:26:46 +0900

    [Bronze II] Title: 공 바꾸기, Time: 196 ms, Memory: 18524 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = i+1;
-        }
-
-        for (int i = 0; i < M; i++) {
-            int tmp;
-            int F = sc.nextInt();
-            int E = sc.nextInt();
-
-            tmp = arr[F-1];
-            arr[F-1] = arr[E-1];
-            arr[E-1] = tmp;
-        }
-        sc.close();
-
-        for (int baguni : arr)
-            System.out.print(baguni + " ");
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+        
+		for(int i=0; i<N; i++) {
+			arr[i] = i+1;
+		}
+		
+		for(int i=0; i<M; i++) {
+			int tmp;
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			tmp = arr[a-1];
+			arr[a-1] = arr[b-1];
+			arr[b-1] = tmp;
+		}
+        
+		for(int i=0; i<N; i++) {
+			System.out.print(arr[i] + " ");
+		}		
+	}
 }
