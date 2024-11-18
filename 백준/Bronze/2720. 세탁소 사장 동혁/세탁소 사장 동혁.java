ec5a67e436730a8c4d3967516e2ce610279a8383
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2024-11-18 19:11:55 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 144 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int[] changes = {25, 10, 5, 1};
-		int T = sc.nextInt();
 
-		for(int i=0; i<T; i++) {
-			int C = sc.nextInt();
-			int[] result = new int[4];
-			
-			for(int j=0; j < changes.length; j++) {
-				result[j] = C/changes[j];
-				C %= changes[j];
-			}
-			
-			for(int res : result) {
-				System.out.print(res + " ");
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int[] coins = {25, 10, 5, 1};
+
+        for(int i=0; i<T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int m = 0;
+            
+            StringBuffer sb = new StringBuffer();
+            for(int j=0; j<coins.length; j++) {
+                m = n / coins[j];
+                n = n % coins[j];
+                sb.append(m + " ");
+            }
+            System.out.println(sb);
+        }
+    }
 }
+
