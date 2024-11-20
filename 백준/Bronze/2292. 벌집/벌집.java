0967752ce1be189030e287813d948660ff8a8ae5
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2024-11-20 21:06:32 +0900

    [Bronze II] Title: 벌집, Time: 176 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int result = 1; 
+		int cnt = 1; 
+		int sum = 6;
+
+		while (true) {
+			if (n <= cnt) {
+				break;
+			}
+			cnt += sum;
+			sum += 6;
+			result++;
+		}
+		System.out.println(result);
+	}
+}
