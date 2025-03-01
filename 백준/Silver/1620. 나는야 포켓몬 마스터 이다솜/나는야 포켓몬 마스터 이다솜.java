7deb53b377120eff7e842504089816ffb041a056
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-03-01 23:29:16 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1260 ms, Memory: 120548 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> nameToNum = new HashMap<>();
+        HashMap<String, String> numToName = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String s = br.readLine();
+            String num = String.valueOf(i);
+            numToName.put(num, s);
+            nameToNum.put(s, num);
+        }
+
+        for (int i = 0; i < m; i++) {
+            String query = br.readLine();
+
+            if (query.matches("\\d+")) {
+                System.out.println(numToName.get(query));
+            } else {
+                System.out.println(nameToNum.get(query));
+            }
+        }
+    }
+}
