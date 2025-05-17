da54b4498ff142da46882976d83e78071c6165a6
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-05-18 03:20:57 +0900

    [Silver V] Title: 뒤집기, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,30 +1,29 @@
+
 import java.util.Scanner;
 
 public class Main {
 	
 	public static int solution(String str) {
-		int count_to_all_zero = 0; // 전체를 0으로 만들기 위한 작업
-		int count_to_all_one = 0; // 전체를 1로 만들기 위한 작업
 		
+		int all_zero_count = 0; // 모두 0으로 만든다.
+		int all_one_count = 0; // 모두 1로 만든다.
 		char[] arr = str.toCharArray();
 		
-		if(arr[0] == '0') {
-			count_to_all_one += 1;
+		if(arr[0] =='0') {
+			all_one_count += 1;
 		} else if (arr[0] == '1') {
-			count_to_all_zero += 1;
+			all_zero_count += 1;
 		}
-		
-		for(int i = 0; i < str.length()-1; i++) {
+		for(int i = 0; i < arr.length-1; i++) {
 			if(arr[i] != arr[i+1]) {
 				if(arr[i+1] == '0') {
-					count_to_all_one += 1;
-				}
-				if(arr[i+1] == '1') {
-					count_to_all_zero += 1;
+					all_one_count++;
+				} else if(arr[i+1] =='1') {
+					all_zero_count++;
 				}
 			}
 		}
-		return Math.min(count_to_all_zero, count_to_all_one);
+		return Math.min(all_zero_count, all_one_count);
 	}
 
 	public static void main(String[] args) {
