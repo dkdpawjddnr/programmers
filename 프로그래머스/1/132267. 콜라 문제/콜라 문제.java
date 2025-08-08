edcba7656918700c9ff177074265f5abba464101
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-08-08 22:44:25 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int solution(int a, int b, int n) { 
+        
+        int result = 0;
+        
+        while(n>=a){
+            result = result + (n / a)*b;
+            n = ((n / a)*b) + (n % a);
+        }
+    
+        return result;
+    }
+}
