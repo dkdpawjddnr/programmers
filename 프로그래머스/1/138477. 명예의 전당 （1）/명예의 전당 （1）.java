8aec22fe10025b3eaa0363fd17212dd23ccc61eb
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-08-20 21:43:00 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.14 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+   
+        ArrayList<Integer> rank = new ArrayList<Integer>();
+        for(int i=0;i<score.length;i++){
+            rank.add(score[i]);
+            Collections.sort(rank);
+            
+            if(i>=k) rank.remove(0);
+            answer[i] = rank.get(0);
+        }
+        return answer;
+    }
+}
