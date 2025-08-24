0f8f11891741f74a27fd3fb0e49cbf3747d1fffd
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-08-25 00:35:14 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.46 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ 
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        String[] mbti = "RTCFJMAN".split("");
+        
+        for(String s : mbti){
+            map.put(s, 0);
+        }
+        
+        for(int i=0; i<survey.length; i++){
+            String sur = survey[i];
+            int cho = choices[i];
+            
+            if(cho < 4){
+                map.put(String.valueOf(sur.charAt(0)), map.get(String.valueOf(sur.charAt(0))) + (4 - cho));
+            }else if(cho > 4){
+                map.put(String.valueOf(sur.charAt(1)), map.get(String.valueOf(sur.charAt(1))) + (cho - 4));
+            }
+        }
+        
+        answer += map.get("R") >= map.get("T") ? "R" : "T";
+        answer += map.get("C") >= map.get("F") ? "C" : "F";
+        answer += map.get("J") >= map.get("M") ? "J" : "M";
+        answer += map.get("A") >= map.get("N") ? "A" : "N";
+        
+        System.out.println(answer);
+        
+        return answer;
+    }
+}
