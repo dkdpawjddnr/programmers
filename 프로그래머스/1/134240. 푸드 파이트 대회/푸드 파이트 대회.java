2cf61db54865cb166960253c7a8766b7a64fdcbc
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-10-14 22:56:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 26.62 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        String result = "0";
+        
+        for(int i=food.length-1;i>0;i--){
+            String prefix = "";
+            String suffix = "";
+            for(int j=0;j<food[i]/2;j++){
+                prefix += i;
+                suffix += i;
+            }
+            result = prefix + result + suffix ;
+        }
+        return result;
+    }
+}
