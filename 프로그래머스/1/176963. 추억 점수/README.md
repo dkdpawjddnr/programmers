c0af2f702782240e413419fa00a3ebda9fa38d7a
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2025-11-17 23:54:12 +0900

    [level 1] Title: 추억 점수, Time: 2.66 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/README.md
+++ 프로그래머스/1/176963. 추억 점수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 추억 점수 - 176963 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963) 
 
 ### 성능 요약
 
-메모리: 109 MB, 시간: 1.47 ms
+메모리: 93.1 MB, 시간: 2.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 30일 17:06:32
+2025년 11월 17일 23:54:06
 
 ### 문제 설명
 
--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,24 +1,22 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-	public int[] solution(String[] name, int[] yearning, String[][] photo) {
-		int[] answer = {};
-		answer = new int[photo.length];
-
-		HashMap<String, Integer> score = new HashMap<String, Integer>();
-		for(int i=0; i<name.length; i++) {
-			score.put(name[i], yearning[i]);
-		}
-
-		for(int i=0; i< photo.length; i++) {
-			int thisPhotoScore = 0;  
-			for(int j=0; j<photo[i].length; j++) {
-				if(score.get(photo[i][j]) != null) {
-					thisPhotoScore += score.get(photo[i][j]);
-				}
-			}
-			answer[i] = thisPhotoScore;
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
         }
         
-		return answer;
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String plus : photo[i]) {
+                sum += map.getOrDefault(plus, 0);
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
     }
 }
