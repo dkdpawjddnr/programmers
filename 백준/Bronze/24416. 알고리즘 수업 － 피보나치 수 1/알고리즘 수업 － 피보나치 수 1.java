d0ded1680049677d8c306f732c9fc8733df4ea71
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-02-25 23:03:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 428 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
     static int cnt1 = 1;
     static int cnt2 = 0;
     public static void main(String args[]) throws IOException{
