db894889e98bf0e80360c0bea28f45cc074b1fa0
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-02-27 23:44:50 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 15900 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,27 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] basket = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            basket[i] = i;
-        }
-        for (int x = 0; x < m; x++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            for (int y = 0; y < (j - i) / 2 + 1; y++) {
-                int tmp = basket[i + y];
-                basket[i + y] = basket[j - y];
-                basket[j - y] = tmp;
-            }
-        }
-        for (int i = 1; i <= n; i++) {
-            System.out.print(basket[i] + " ");
-        }
-
-        sc.close();
-    }
+   
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st = new StringTokenizer( br.readLine() );
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
+  
+      int[] num = new int[N];
+      
+      for(int i = 0; i < N;i++) {
+    	  num[i] = i+1;
+      }
+      for(int i = 0; i < M;i++) {
+    	  st = new StringTokenizer(br.readLine());
+    	  int A = Integer.parseInt(st.nextToken());
+    	  int B = Integer.parseInt(st.nextToken());
+    	  int[] copy_num = Arrays.copyOfRange(num, A-1, B);
+    	  int c = copy_num.length;
+    	  for(int j = A-1; j < B; j++)
+    	  {
+    		num[j] = copy_num[c-1];
+    		c--;
+    	  }
+      }
+      for(int j = 0; j < N;j++) {
+    	  bw.write(num[j] + " ");
+      }
+      
+      bw.flush();
+      bw.close();
+      br.close();
+   }   
 }
