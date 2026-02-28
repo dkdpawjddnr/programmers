30526d06fa0cf8b72d5450a62eed77b040d7d3a9
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-02-28 20:28:48 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/README.md
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 9번 / 이웃한 칸 - 250125 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250125) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250125?language=java) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.03 ms
+메모리: 76.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 19일 23:11:16
+2026년 02월 28일 20:28:45
 
 ### 문제 설명
 
--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,24 +1,22 @@
-import java.util.*;
-
 class Solution {
-    static int[] dh= {-1, 1, 0, 0};
-    static int[] dw= {0, 0, -1, 1};
-    
     public int solution(String[][] board, int h, int w) {
-        int answer = 0;
-        int r = board.length;
-        int c = board[0].length;
-        
-        for(int i=0;i<4;i++) {
-            int next_h= h + dh[i];
-            int next_w= w + dw[i];
-            if(next_h < 0 || next_h >= r || next_w < 0 || next_w >= c) 
-                continue;
-            if(board[h][w].equals(board[next_h][next_w])) {
-                answer ++;
+        int n = board.length;
+        int count = 0;
+        String targetColor = board[h][w];
+
+        int[] dh = {0, 1, -1, 0};
+        int[] dw = {1, 0, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int h_check = h + dh[i];
+            int w_check = w + dw[i];
+
+            if (h_check >= 0 && h_check < n && w_check >= 0 && w_check < n) {
+                if (board[h_check][w_check].equals(targetColor)) {
+                    count++;
+                }
             }
         }
-        return answer;
+        return count;
     }
 }
- 
