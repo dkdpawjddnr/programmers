6af1d9de43728b44a49c4beb010da8382c70d496
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-03-01 23:46:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,36 +1,34 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int total = Integer.parseInt(sc.nextLine());
-		
-		int cnt = 0;
-		
-		for(int k = 0 ; k < total ; k++) {
-			boolean flag = false;
-			String input = sc.nextLine();
-			
-			for(int i = 0 ; i < input.length() ; i++) {
-				char x = input.charAt(i);
-				int start = input.indexOf(x);
-				int end = input.lastIndexOf(x) + 1;
-				
-				String result = "";
-				
-				for(int j = 0 ; j < end - start ; j++) {
-					result += x;
-				}
-				
-				if(!input.substring(start, end).equals(result)) {
-					flag = true;
-					break;
-				}
-			}
-			if(!flag) {
-				cnt++;
-			}
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); 
+        int count = N;
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            int prev = 0;
+
+            int[] alphabet = new int[26];
+
+            for (int j = 0; j < str.length(); j++) {
+                int now = str.charAt(j);
+                
+                if (prev != now) {
+                    if (alphabet[now - 97] == 0) {
+                        alphabet[now - 97]++;
+                        prev = now;
+                    } else {
+                        count--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
 }
