e4f9a3b66b885f06adb0b3da04c406b5e47befd1
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-03-02 21:27:50 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,12 +1,23 @@
 import java.io.*;
-import java.util.*;
-public class Main {
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-        System.out.print(Math.max(a,b));
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+    
+        if(A<B)
+            bw.write(String.valueOf(B));
+        else
+            bw.write(String.valueOf(A));
+
+
+        bw.flush();
+        bw.close();
     }
 }
