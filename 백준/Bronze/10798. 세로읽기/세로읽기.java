277008762b3c261563a1d2d87843d332020b431f
지정욱 <90197017+dkdpawjddnr@users.noreply.github.com>
2026-03-06 22:51:24 +0900

    [Bronze I] Title: 세로읽기, Time: 140 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
 
-public class Main
-{
-	public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-        String[] input_ = new String[5];
-        String result = "";
+public class Main {
+    static ArrayList<String> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-		for (int i = 0; i < 5; i++) {
-        	input_[i] = sc.next();
-		}
-		int flag; int index = 0;
-		while(true) {
-			flag = 0;
-			for (int i = 0; i < 5; i++) {
-				if (index < input_[i].length()) {
-					flag = 1;
-					result += input_[i].charAt(index);
-				}
-			}
-			if (flag == 0) break;
-			index++;
-		}
-		System.out.println(result);
-	}
+        for(int i=0;i<5;i++){
+            String input = br.readLine();
+            for(int j=0;j<input.length();j++){
+                if(list.size()<=j)
+                    list.add(input.charAt(j)+"");
+                else		
+                    list.set(j,list.get(j) + input.charAt(j));
+            }
+        }
+        for(int i=0;i<list.size();i++)
+            bw.write(list.get(i));
+        
+        bw.flush();		
+        bw.close();
+        br.close();
+    }
 }
+
